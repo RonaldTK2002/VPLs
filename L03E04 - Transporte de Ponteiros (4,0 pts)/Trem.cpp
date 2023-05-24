834f19819b2b5b133c8ebd3ecabe588816e45ae9
Ronaldo <ronaldodutra777@gmail.com>
2023-05-23 21:59:45 -0300

    Termiando configs

--- L03E04 - Transporte de Ponteiros (4,0 pts)/Trem.cpp
+++ L03E04 - Transporte de Ponteiros (4,0 pts)/Trem.cpp
@@ -5,13 +5,12 @@
 #include <string>
 #include <iostream>
 
-
-
-
-bool Trem::pagar_passagem(std::shared_ptr<Passageiro> passageiro){
-    bool tem_vagas = this->
-    bool tem_dinheiro = passageiro->get_saldo() > this-> _valor_passagem;
-    if(tem_vagas&&tem_dinheiro){
-        passageiro->descontar_valor_passagem(this->_valor_passagem)
+bool Trem::pagar_passagem(std::shared_ptr<Passageiro> passageiro)
+{
+    bool tem_vagas = (this->_capacidade) > (this->_passageiros.size());
+    bool tem_dinheiro = (passageiro->get_saldo()) > (this->_valor_passagem);
+    if (tem_vagas && tem_dinheiro)
+    {
+        passageiro->descontar_valor_passagem(this->_valor_passagem);
     }
 }
